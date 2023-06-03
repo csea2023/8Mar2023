266fe7631d378248f2de56ff2a6c383b6212ed01
Kaustubh S <kasben10@gmail.com>
2023-06-03 11:21:58 +0530

    Third commit

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-function submitForm() {
-    var fileInput = document.getElementById('fileip');
-    var file = fileInput.files[0];
-    
-    if (file.type.indexOf('audio') === -1) {
-      alert('Please select an audio file');
-      return false;
-    }   
-    return true;
+function validateForm() {
+  var fileInput = document.getElementById('fileip');
+  var file = fileInput.files[0];
+
+  if (file) {
+    var fileName = file.name;
+    var fileExtension = fileName.split('.').pop().toLowerCase();
+
+    if (fileExtension === 'mp3') {
+
+      console.log('File is valid.');
+      // Perform any desired action for a valid MP3 file
+    }
+    else {
+      // Invalid file type
+      alert('Please upload an MP3 file.');
+
+      // Reset the file input field
+      fileInput.value = '';
+    }
   }
+}
